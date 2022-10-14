998ba89e6b8c009a9928c1c08ccb76b8756e7092
angle-zhang <angelazhang1220@gmail.com>
2022-10-14 12:31:24 -0400

    japan other food data

--- step1_clean_datasets.R
+++ step1_clean_datasets.R
@@ -17,10 +17,68 @@ japan_nutrients2 <- read_excel("./data-raw/Japan/Japan_fattyacid_values.xlsx")[,
 taxa_table <- readRDS("data-raw/taxa_table.Rds")
 
 
+# clean japan food comp data  
+rename_columns <- function (name_row_idx, df) { 
+  japan_colnames <- unname(unlist(df[name_row_idx,]))
+  colnames(df) <- tolower(make.names(japan_colnames))
+  
+  return(df)
+}
+
+japan_scinames = rename_columns(2, japan_scinames)
+japan_nutrients = rename_columns(4, japan_nutrients)
+japan_nutrients1 = rename_columns(4, japan_nutrients1)
+japan_nutrients2 = rename_columns(4, japan_nutrients2)
+
+## OVERALL DIETARY DATA
+# merge other data sources (other nturients)
+jap_nutr = japan_nutrients %>% merge(japan_nutrients1, by=c("item...no.", "water", "food.and.description"), all=T) %>%
+  merge(japan_nutrients2, by.x=c("item...no.", "water", "food.and.description"), by.y=c("item.no.", "water", "food.and.description"), all=T) %>%
+  select(-c(lipid.x)) %>%
+  select(1, 3:6, water, everything()) %>%
+  rename(id=item...no.,
+         food_desc=food.and.description,
+         food_group=food...group) %>%
+  gather("nutrient", "value", 6:153) %>%
+  mutate(food_group=as.numeric(food_group)) %>%
+  mutate(food_group=recode(food_group,
+                           `1`='Cereals',
+                           `2`='Potatoes and starches', 
+                           `4`='Pulses',
+                           `5`='Nuts and seeds', 
+                           `6`='Vegetables', 
+                           `8`='Mushroom',
+                           `9`='Algae',
+                           `10`='Fish, mollusks, and crustacean',
+                           `11`="Meat",
+                           `12`="Eggs"))
+
+## get units key
+units_key <- jap_nutr[jap_nutr$food_desc=="Unit",] %>%
+  drop_na(food_desc) %>%
+  select(nutrient, units)
+
+jap_nutr = jap_nutr %>%
+  merge(units_key, by="nutrient") %>%
+  subset(value!="-") %>% # clean values
+  mutate(value=gsub("[()]", "", value)) %>% #parentheses denote estimated value
+  mutate(trace=case_when(value=="Tr" ~ TRUE, 
+                         TRUE ~ FALSE),
+         value=case_when(value=="Tr" ~ "0", 
+                         TRUE ~ value),
+         value=as.numeric(value)
+  )  %>% #indicate if trace values present
+  mutate(nutrient=(str_replace_all(nutrient, "\\.", " ") %>% str_squish())) %>%
+  select(-index...no.) %>%
+  rename(notes=remarks.y)
+  
+
+
+## ALGAE DATA
 #filter out algae & get same genuses from AFCD 
 japan_sciname_key <- japan_scinames %>%
+  filter(grepl("Algae", english.name.of.food.source)) %>%
   drop_na(scientific.name) %>%
-  filter(grepl('Algae',english.name.of.food.source)) %>%
   separate(english.name.of.food.source, into=c("algae", "common_name1", "common_name2"), sep=", ") %>%
   separate(scientific.name, into=paste0("scientific_name", 1:2), sep="\\[Syn. ") %>%
   mutate(genus=word(scientific_name1, 1)) %>%
@@ -34,61 +92,24 @@ japan_sciname_key <- japan_scinames %>%
   drop_na(id)
 
 
-# clean japan food comp data  
-rename_columns <- function (name_row_idx, df) { 
-  japan_colnames <- unname(unlist(df[name_row_idx,]))
-  colnames(df) <- tolower(make.names(japan_colnames))
-  
-  return(df)
-}
-
-japan_scinames = rename_columns(2, japan_scinames)
-japan_nutrients = rename_columns(4, japan_nutrients)
-japan_nutrients1 = rename_columns(4, japan_nutrients1)
-japan_nutrients2 = rename_columns(4, japan_nutrients2)
-#japan_nutrients3 = rename_columns(3, japan_nutrients3)
- 
-
-jap_nutr = japan_nutrients %>% merge(japan_nutrients1, by=c("item...no.", "water", "food.and.description"), all=T) %>%
-  merge(japan_nutrients2, by.x=c("item...no.", "water", "food.and.description"), by.y=c("item.no.", "water", "food.and.description"), all=T) %>%
-  select(-c(lipid.x)) %>%
-  select(1, 3:6, water, everything()) 
-                              
-
-## filter japan_nutrients for kelp values 
-## gather data 
-jap_nutr2 <- jap_nutr %>%
-  rename(id=item...no.) %>%
-  gather("nutrient", "value", 6:153) %>%
-  filter(id %in% japan_sciname_key$id) 
-
-## get units key
-units_key <- jap_nutr[jap_nutr$food.and.description=="Unit",] %>%
-  drop_na(food.and.description) %>%
-  gather(nutrient, units, 6:153)%>%
-  select(nutrient, units)
+## filter japan nutrients for non-kelp values
+jap_food_data <- jap_nutr %>%
+  filter(food_group!="Algae") %>%
+  drop_na(food_group)
 
 ## merge scientific data 
-jap_data <- japan_sciname_key %>%
-  merge(jap_nutr2, by="id") %>%
-  merge(units_key, by="nutrient") %>%
+jap_algae_data <- jap_nutr %>%
+  filter(food_group=="Algae") %>%
+  # merge by common name instead 
+  mutate(common_name1=tolower(word(food_desc, 2, sep=", "))) %>%
+  mutate(common_name1=(str_replace_all(common_name1, "\"", " ") %>% str_squish())) %>%
+  merge(japan_sciname_key, by="common_name1") %>%
   merge(taxa_table, by="genus") %>% # merge nutr data with taxa table to get broader groups
-  mutate(nutrient=(str_replace_all(nutrient, "\\.", " ") %>% str_squish())) %>%
-  select(-c(food...group, index...no.)) %>%
-  rename(notes=remarks.y,
-         food_desc=food.and.description) %>%
   # classify nutr types 
+  # TODO add green algae
   mutate(algae_type=case_when(class=="phaeophyceae" ~ "brown",
                               phylum=="rhodophyta" ~ "red",
-                              genus=="spirulina" ~ "blue-green")) %>%
-  subset(value!="-") %>% # clean values
-  mutate(value=gsub("[()]", "", value)) %>% #parentheses denote estimated value
-  mutate(trace=case_when(value=="Tr" ~ TRUE, 
-                         TRUE ~ FALSE),
-         value=case_when(value=="Tr" ~ "0", 
-                         TRUE ~ value),
-         value=as.numeric(value)
-  )  #indicate if trace values present
+                              genus=="spirulina" ~ "blue-green")) 
 
 
 
@@ -103,6 +124,9 @@ jap_data <- japan_sciname_key %>%
 
 # get nutrients of interest (from AFCD?)
 
-write.csv(jap_data, "./data/jap_nutr.csv")
+write.csv(jap_algae_data, "./data/jap_nutr.csv")
+write.csv(jap_food_data, "./data/jap_nutr2.csv")
+
+
   
 
--- step2_visualize_nutrients.R
+++ step2_visualize_nutrients.R
@@ -7,6 +7,19 @@ library(readxl)
 
 jap_nutr <- read.csv("./data/jap_nutr.csv")
 nutr_key <- read_excel('./data-raw/nutr_key.xlsx') 
+dri_data <- read.csv('./data-raw/dri_nutrients.csv')
+
+### helper functions
+convert_units <- function(value, unit_orig, unit_new) {  
+  if(unit_new=="" | is.na(unit_new)) {
+    return(value)
+  }
+  else { 
+    unit_conversionf <- c("g"=1, "mg"=1000, "mcg"=1000000, "ug"=1000000, "µg"=1000000)
+    result <- unname(value*unit_conversionf[unit_new]/unit_conversionf[unit_orig])[1]
+    return(result)
+  }
+}
 
 nutr_key <- nutr_key %>% select(1,2,10) 
 
@@ -16,16 +29,102 @@ names(nutr_key) <- c('nutrient', "japan.nutrient", "ph_ranking")
 nutr_names <- jap_nutr %>% select(nutrient) %>% unique()
 print(nutr_names)
 
-jap_nutr <- jap_nutr %>% merge(nutr_key, by.x="nutrient", by.y="japan.nutrient") %>%
+# rename & fix units
+jap_nutr1 <- jap_nutr %>% 
+  merge(nutr_key, by.x="nutrient", by.y="japan.nutrient") %>%
   rename(nutrient_orig=nutrient) %>%
   rename(nutrient=nutrient.y) %>%
+  mutate(units=gsub('\\/100 g', "", units)) %>%
   select(X, id, nutrient, everything()) 
 
+convert_units  <- Vectorize(convert_units)
+
+## get units and nutrients from DRI 
+nutr_unit_key = dri_data %>%
+  select(nutrient, nutrient_units) %>%
+  rename(unit_conversion=nutrient_units) %>%
+  filter(nutrient!="Protein") %>% # removing protein for now
+  unique() 
+
+jap_nutr2 = jap_nutr1 %>% 
+  merge(nutr_unit_key) %>%
+  mutate(value=convert_units(value, units, unit_conversion)) %>%
+  mutate(units_orig=units, 
+         units=unit_conversion) %>%
+  select(-unit_conversion)
+
+# get UL bounds with descriptions of groups (age, sex, etc)
+ref_ul <- dri_data %>% 
+  filter(units!="g/kg" | nutrient!="Protein" ) %>%
+  group_by(nutrient, dri_type) %>%
+  mutate(
+    max_ref = max(ref_value),
+    min_ref = min(ref_value), # min or max 
+    mean_ref = mean(ref_value)
+  ) %>%
+  gather("summary_type", "value", min_ref, max_ref, mean_ref) %>%
+  filter(summary_type=="mean_ref"| ref_value==value | ref_value==value) %>%
+  mutate(across(c(sex_stage, sex, stage, age_range), ifelse, test=summary_type=="mean_ref", yes=NA)) %>%
+  select(-data_type, -relevance, -X, -ref_value, -ref_units, -units) %>%
+  rename(units=nutrient_units) %>%
+  ungroup() %>%
+  unique()
+
+
+# get summary UL bounds without descriptions of groups 
+ref_ul_summary <- ref_ul %>%
+  select(-age_range, -sex, -sex_stage, -stage) %>%
+  unique()
+
+t <- ref_ul_summary %>%
+  select(nutrient) %>%
+  unique()
+  
+
+jap_dri1 = jap_nutr2 %>% 
+  group_by(nutrient) %>%
+  mutate(n = n()) %>%
+  merge(ref_ul_summary, by=c("nutrient", "units")) %>%
+  mutate(ID = row_number(), 
+         proportion = value.x/value.y) %>%
+  select(ID, everything()) 
+
 # all seaweeds 
-jap_nutr %>%
+jap_nutr1 %>%
   ggplot(aes(x=nutrient, y=value, fill=nutrient)) +
   geom_boxplot() +
   scale_y_log10() 
+
+# DRi of seaweeds 
+jap_dri1 %>%
+  filter(dri_type=="AI", summary_type=="mean_ref") %>%
+  ggplot(aes(x=reorder(nutrient, proportion), y=proportion, fill=nutrient)) +
+  geom_boxplot() +
+  geom_hline(yintercept=1, color="red") + 
+  scale_y_log10() + 
+  coord_flip() 
+
+# AI by seaweed type
+jap_dri1 %>%
+  filter(dri_type=="AI", summary_type=="mean_ref") %>%
+  drop_na(algae_type) %>%
+  ggplot(aes(x=reorder(nutrient, proportion), y=proportion, fill=algae_type)) +
+  geom_boxplot() +
+  geom_hline(yintercept=1, color="red") + 
+  scale_y_log10() + 
+  coord_flip()
+
+# UL by seaweed type
+jap_dri1 %>%
+  filter(dri_type=="UL", summary_type=="mean_ref") %>%
+  drop_na(algae_type) %>%
+  ggplot(aes(x=reorder(nutrient, proportion), y=proportion, fill=nutrient)) +
+  geom_boxplot() +
+  geom_hline(yintercept=1, color="red") + 
+  scale_y_log10() + 
+  coord_flip() + 
+  facet_wrap(~algae_type)
+
 
 
 
@@ -33,3 +132,4 @@ jap_nutr %>%
 
 
 # all raw seaweds
+
